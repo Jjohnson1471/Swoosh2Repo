eb160cfd0e684c4fdae1dcb7d86af1363be75b3b
Joshua Johnson <joshua.johnson6@csuglobal.edu>
2020-02-17 18:22:18 -0800

    This is a test

--- app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -8,5 +8,7 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val test = "Your mamam"
     }
 }
