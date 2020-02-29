2cdf602ab74431e71dbeba96608da35a06ea1a21
Joshua Johnson <joshua.johnson6@csuglobal.edu>
2020-02-29 08:51:14 -0800

    Added Override SavedInstance State

--- app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -3,6 +3,7 @@ package com.example.swoosh.Controller
 import Model.Player
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.swoosh.R
@@ -13,11 +14,25 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
         }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
+    }
+
 
     fun onMensClicked(view: View) {
         womensLeagueButton.isChecked = false
@@ -47,7 +62,7 @@ class LeagueActivity : BaseActivity() {
         if (player.league != "") {
             val skillIntent = Intent(this, SkillActivity::class.java)
             skillIntent.putExtra(EXTRA_PLAYER, player)
-            
+
             startActivity(skillIntent) } else {
             Toast.makeText(this, "Please make a selection before continuing", Toast.LENGTH_SHORT).show()
         }
